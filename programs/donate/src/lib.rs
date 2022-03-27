6baeeb45cfe33e2883a85d8694f1aa41d8018a48
Javed <jaybee.codes@gmail.com>
2022-03-27 14:08:42 +0300

    WIP: donate_program instruction

---
+++ programs/donate/src/lib.rs
@@ -0,0 +1,34 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{system_instruction, program::{ invoke }};
+
+declare_id!("HH95eqLi8izRFNVob95g4URG62Jkf9HmtXLz5MFXyh7z");
+
+#[program]
+pub mod donate {
+    use super::*;
+    
+    pub fn donate_program(ctx: Context<Donation>, donation_amount: u64) -> Result<()> {
+        let sender = &ctx.accounts.sender.key();
+        let program_id = &ctx.accounts.program_account.key();
+
+        let instruction = system_instruction::transfer(sender, program_id, donation_amount);
+
+        invoke(&instruction, &[ctx.accounts.sender.to_account_info(), ctx.accounts.program_account.to_account_info()])?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Donation<'info> {
+    #[account(mut)]
+    pub program_account: Account<'info, ProgramAccount>,
+    #[account(mut)]
+    pub sender: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct ProgramAccount {
+    pub pub_key: Pubkey
+}
---
+++ tests/donate.ts
@@ -0,0 +1,27 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Donate } from "../target/types/donate";
+
+describe("donate", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Donate as Program<Donate>;
+
+  it("donates lamports to program's account", async () => {
+    const LAMPORTS_TO_DONATE = new anchor.BN(100);
+    const donater = anchor.web3.Keypair.generate();
+
+    await program.rpc.donateProgram(LAMPORTS_TO_DONATE, {
+      accounts: {
+        sender: donater.publicKey,
+        programAccount: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [donater]
+    })
+
+    const programAccount = await program.account.programAccount.fetch(program.programId);
+  	console.log(programAccount);
+  })
+});
